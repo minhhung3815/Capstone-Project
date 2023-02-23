9cedbe8beda9ffdb8115a6454390ec669c5ba85c
minhhung3815 <minhhungnguyen3815@gmail.com>
2023-02-23 13:40:32 +0700

    Update api route name

--- app/router/index.js
+++ app/router/index.js
@@ -6,13 +6,13 @@ const doctorRouter = require('./doctorRouter');
 const accountRouter = require('./accountRouter');
 const router = express.Router();
 
-router.use('/api/v1/account', accountRouter);
+router.use('/account', accountRouter);
 
-router.use('/api/v1/doctor', doctorRouter);
+router.use('/doctor', doctorRouter);
 
-router.use('/api/v1/user', userRouter);
+router.use('/user', userRouter);
 
-router.use('/api/v1/admin', adminRouter);
+router.use('/admin', adminRouter);
 
 router.use(error404Router);
 
