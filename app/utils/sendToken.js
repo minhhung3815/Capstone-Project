e12dd48c05439f2754cbafde2330ac462f4f5ef3
minhhung3815 <minhhungnguyen3815@gmail.com>
2023-04-30 15:29:17 +0700

    Modify vercel

--- app/utils/sendToken.js
+++ app/utils/sendToken.js
@@ -8,7 +8,8 @@ const sendToken = async (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
     ),
-    httpOnly: false,
+    domain: "vercel.app",
+    httpOnly: true ,
     sercue: false,
   };
 
