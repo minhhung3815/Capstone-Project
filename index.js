e0e31c596ff02246f2f800c53b185f98ca6f48b9
minhhung3815 <minhhungnguyen3815@gmail.com>
2023-04-30 13:54:12 +0700

    Modify vercel

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: ["http://localhost:3000"] }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
